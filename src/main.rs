fb6e3909aba7b303b8da04f3327591c50b007d64
GokceOnal1 <113534282+GokceOnal1@users.noreply.github.com>
2024-10-29 17:51:23 -0400

    command-line things

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,17 @@ use std::rc::Rc;
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
 
+    let args: Vec<String> = std::env::args().collect();
 
+    if args.len() != 3 {
+        panic!("Command-Line Error: Expected 2 command line arguments: 'gscript [filename]'");
+    }
+    if args[1] != "gscript" {
+        panic!("Command-Line Error: No support for commands other than 'gscript' has been implemented");
+    }
+    let filename = args[2].clone();
     let errorstack = Rc::new(RefCell::new(ErrorStack::new()));
-    let mut lexer = Lexer::new("entry/entry.gsc", Rc::clone(&errorstack));
+    let mut lexer = Lexer::new(format!("entry/{}", filename).as_str(), Rc::clone(&errorstack));
     lexer.lex();
 
     let mut parser = Parser::new(&lexer.tokens, Rc::clone(&errorstack));
